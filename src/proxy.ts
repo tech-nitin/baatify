9e356440c9a44f87c61d50bf2aec65aba643b8b5
Nitin <technitin007@gmail.com>
2026-06-15 21:48:08 +0530

    fix proxy for Next.js 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,30 +1,46 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
-};
+export async function proxy(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
   if (
     token &&
-    (url.pathname.startsWith('/sign-in') ||
+    (
+      url.pathname.startsWith('/sign-in') ||
       url.pathname.startsWith('/sign-up') ||
       url.pathname.startsWith('/verify') ||
-      url.pathname === '/')
+      url.pathname === '/'
+    )
   ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(
+      new URL('/dashboard', request.url)
+    );
   }
 
-  if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
+  if (
+    !token &&
+    url.pathname.startsWith('/dashboard')
+  ) {
+    return NextResponse.redirect(
+      new URL('/sign-in', request.url)
+    );
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    '/dashboard/:path*',
+    '/sign-in',
+    '/sign-up',
+    '/',
+    '/verify/:path*',
+  ],
+};
