76ba7d3c1e1bf9cfb06934c304c8bd515cccad32
Nitin <technitin007@gmail.com>
2026-06-15 22:38:56 +0530

    fix all sender

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -10,37 +10,70 @@ export const authOptions: NextAuthOptions = {
       id: 'credentials',
       name: 'Credentials',
       credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials: any): Promise<any> {
-        await dbConnect();
-        try {
-          const user = await UserModel.findOne({
-            $or: [
-              { email: credentials.identifier },
-              { username: credentials.identifier },
-            ],
-          });
-          if (!user) {
-            throw new Error('No user found with this email');
-          }
-          if (!user.isVerified) {
-            throw new Error('Please verify your account before logging in');
-          }
-          const isPasswordCorrect = await bcrypt.compare(
-            credentials.password,
-            user.password
-          );
-          if (isPasswordCorrect) {
-            return user;
-          } else {
-            throw new Error('Incorrect password');
-          }
-        } catch (err: any) {
-          throw new Error(err);
-        }
-      },
+  identifier: {
+    label: 'Email or Username',
+    type: 'text',
+  },
+
+  password: {
+    label: 'Password',
+    type: 'password',
+  },
+},
+      async authorize(credentials: any) {
+  await dbConnect();
+
+  try {
+
+    console.log('Credentials:', credentials);
+
+    const user = await UserModel.findOne({
+      $or: [
+        { email: credentials.identifier },
+        { username: credentials.identifier },
+      ],
+    });
+
+    console.log('Found user:', user);
+
+    if (!user) {
+      throw new Error('No user found');
+    }
+
+    console.log('isVerified:', user.isVerified);
+
+    if (!user.isVerified) {
+      throw new Error(
+        'Please verify your account before logging in'
+      );
+    }
+
+    const isPasswordCorrect =
+      await bcrypt.compare(
+        credentials.password,
+        user.password
+      );
+
+    console.log(
+      'Password correct:',
+      isPasswordCorrect
+    );
+
+    if (!isPasswordCorrect) {
+      throw new Error('Incorrect password');
+    }
+
+    return user;
+
+  } catch (err: any) {
+
+    console.log(err);
+
+    throw new Error(
+      err.message || 'Authentication failed'
+    );
+  }
+},
     }),
   ],
   callbacks: {
